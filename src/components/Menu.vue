568ed278eacc4e4e58331a06c8a3d258629cacdf
larrygf02 <raulgf_02@hotmail.com>
2020-04-25 11:16:18 -0500

    Menu with slot

---
+++ src/components/Menu.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-row :align="align">
+        <v-col cols="2">
+            <v-card class="mx-auto" width="300">
+            <v-list>
+                <v-list-item>
+                <v-list-item-icon>
+                    <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-title>Home</v-list-item-title>
+                </v-list-item>
+
+                <v-list-group
+                prepend-icon="mdi-account-circle"
+                value="true"
+                >
+                <template v-slot:activator>
+                    <v-list-item-title>Users</v-list-item-title>
+                </template>
+
+                <v-list-group
+                    no-action
+                    sub-group
+                    value="true"
+                >
+                    <template v-slot:activator>
+                    <v-list-item-content>
+                        <v-list-item-title>Admin</v-list-item-title>
+                    </v-list-item-content>
+                    </template>
+
+                    <v-list-item
+                    v-for="(admin, i) in admins"
+                    :key="i"
+                    link
+                    >
+                    <v-list-item-title v-text="admin[0]"></v-list-item-title>
+                    <v-list-item-icon>
+                        <v-icon v-text="admin[1]"></v-icon>
+                    </v-list-item-icon>
+                    </v-list-item>
+                </v-list-group>
+
+                <v-list-group
+                    sub-group
+                    no-action
+                >
+                    <template v-slot:activator>
+                    <v-list-item-content>
+                        <v-list-item-title>Actions</v-list-item-title>
+                    </v-list-item-content>
+                    </template>
+                </v-list-group>
+                </v-list-group>
+            </v-list>
+            </v-card>
+        </v-col>
+        <v-col cols="10">
+            <slot name="content"></slot>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      admins: [
+        ['Management', 'mdi-account-multiple-outline'],
+        ['Settings', 'mdi-settings'],
+      ],
+      cruds: [
+        ['Create', 'add'],
+        ['Read', 'insert_drive_file'],
+        ['Update', 'update'],
+        ['Delete', 'delete'],
+      ],
+    }),
+  }
+</script>
--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>Bienvenidos user {{ isLogin }}</h1>
+    <div>
+        <menu-vue>
+            <template slot="content">
+                <h1>Bienvenidos user {{ isLogin }}</h1>
+            </template>
+        </menu-vue>
+    </div>
 </template>
 <script>
 import { mapGetters } from 'vuex' 
+import MenuVue from '../components/Menu.vue'
+
 export default {
     name: 'Welcome',
+    components: {
+        MenuVue
+    },
     computed: {
         ...mapGetters('user',['isLogin'])
     }
